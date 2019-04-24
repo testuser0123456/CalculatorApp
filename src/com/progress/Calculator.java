047fb77bea3cb2d64ac82c03e4903969a3ecb622
vbontha <vinodkumar.bontha@progress.com>
2019-04-24 17:51:52 +0530

    Initial committ

---
+++ src/com/progress/Calculator.java
@@ -0,0 +1,21 @@
+package com.progress;
+
+public class Calculator {
+	
+	public long addition(long a, long b) {
+		return a+b;
+	}
+	
+	public long substraction(long a, long b) {
+		return a-b;
+	}
+	
+	public long multiplication(long a, long b) {
+		return a*b;
+	}
+	
+	public long division(long a, long b) {
+		return a/b;
+	}
+
+}
