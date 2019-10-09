a19c5a2c0883afad4b0f43d9459fccdb6cb8ffb4
mkatepal <mkatepal@progress.com>
2019-10-09 14:14:08 +0530

    added test sources

---
+++ src/test/java/com/test/UnrecognizedCases.java
@@ -0,0 +1,39 @@
+package com.test;
+
+import com.progress.Calculator;
+
+import junit.framework.Assert;
+
+public class UnrecognizedCases {
+	
+	private static Calculator calculator = new Calculator();
+	
+	public void testAddition() {
+		System.out.println("##teamcity[testStarted name='testAddition']");
+		Assert.assertEquals(13, calculator.addition(10, 3));
+		System.out.println("##teamcity[testFinished name='testAddition']");
+	}
+	
+	public void testSubstraction() {
+		System.out.println("##teamcity[testStarted name='testSubstraction']");
+		Assert.assertEquals(2, calculator.substraction(5, 3));
+		System.out.println("##teamcity[testFinished name='testSubstraction']");
+	}
+	
+	public void testMultiplication() {
+		System.out.println("##teamcity[testStarted name='testMultiplication']");
+		try {
+			Assert.assertEquals(15, calculator.multiplication(5, 3));
+		}
+		catch(Exception e) {
+			System.out.println("##teamcity[testFailed name='testMultiplication']");
+		}
+	}
+	
+	public static void main(String args[]) {
+		UnrecognizedCases tests = new UnrecognizedCases();
+		tests.testAddition();
+		tests.testSubstraction();
+		tests.testMultiplication();
+	}
+}
