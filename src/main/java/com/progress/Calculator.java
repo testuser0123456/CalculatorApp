3d0af9b0c4a112cad218939ae0cab33d1998a810
mkatepal <mkatepal@progress.com>
2019-10-11 10:03:16 +0530

    hd

--- src/main/java/com/progress/Calculator.java
+++ src/main/java/com/progress/Calculator.java
@@ -1,14 +1,9 @@
 package com.progress;
 
-import org.apache.log4j.Logger;
-
 public class Calculator {
-	
-	Logger logger = Logger.getLogger(Calculator.class);
 
 	
 	public long addition(long a, long b) {
-		logger.info("add");
 		System.out.println("addition");
 		return a+b;
 	}
